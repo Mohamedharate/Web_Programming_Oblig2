c4ab53df2fdc98b3a5f73000f3ad325e896102b8
MOHAMED HARATE <haratemo@gmail.com>
2021-03-08 13:17:44 +0100

    Jobber med styling

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -64,12 +64,12 @@ function hentAlle(){
 
 
 function formaterData(billetter){
-    let ut = "<h1>Alle Billetter</h1>\n<table class='table table-striped table-bordered'><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>Epost</th></tr>";
+    let ut = "\n<table class='table table-striped table-bordered'><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>Epost</th></tr>";
     for (const billett of billetter){
         ut += "<tr><td>"+billett.film+"</td><td>"+billett.antall+"</td><td>"+billett.fornavn+"</td><td>"+billett.etternavn+"</td>" +
             "<td>"+billett.telefonnr+"</td><td>"+billett.mail+"</td></tr>";
     }
-    ut += "</table><button onclick=\"slettBilletter()\" class=\"w-100 btn btn-primary btn-lg\" type=\"submit\">Slett alle</button>"
+    ut += "</table>"
     $("#billettene").html(ut);
 }
 
